be234316eb7bb5e150c52777760000a6b6a7a942
rampa2510 <iamram2510@gmail.com>
2021-03-01 13:52:14 +0530

    Added more solution

---
+++ bitwise/decToBin.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void method2(int n) {
+  int p = 1, ans;
+  while (n > 0) {
+    ans = n & 1;
+    ans += ans * p;
+    p *= 10;
+    n>>=1;
+  }
+  cout << ans << "\n";
+}
+
+int main() {
+  int n, a, b;
+  cin >> n;
+  b = n;
+
+  string ans;
+  while (n > 0) {
+    a = n & 1;
+    /* cout << a << "\n"; */
+    ans = to_string(a) + ans;
+    n >>= 1;
+  }
+  cout << ans << "\n";
+  method2(b);
+  return 0;
+}
--- bitwise/finNoOfSetBits.cpp
+++ bitwise/finNoOfSetBits.cpp
@@ -2,18 +2,28 @@
 
 using namespace std;
 
+void countBits(int n) {
+  int ans = 0;
+  while (n > 0) {
+    n = n & (n - 1);
+    ans++;
+  }
+  cout << ans << "\n";
+}
+
 int main() {
-  int n, c = 0;
+  int n, c = 0, a = 1;
   cin >> n;
   while (true) {
     if (n == 0 || n == 1)
       break;
-    n = n >> 1;
-    c++;
-
+    c += n & a;
+    n >>= 1;
   }
-  if (n )
+  if (n)
     c++;
   cout << c << "\n";
+  /* countBits(n); */
+  /* cout << __builtin_popcount(n) << "\n"; */
   return 0;
 }
